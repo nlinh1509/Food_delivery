73faac099c95b54be2b9acf2f69ef5bc6fd20703
Lê Võ Nhật Linh <levonhatlinh.work@gmail.com>
2025-11-11 13:04:29 +0700

    layout navbar

--- front-end/src/components/Navbar/Navbar.jsx
+++ front-end/src/components/Navbar/Navbar.jsx
@@ -1,8 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Navbar.css";
+import { assets } from "../../assets/assets";
 
 const Navbar = () => {
-  return <div className="navbar"></div>;
+  const [menu, setMenu] = useState("home");
+  return (
+    <div className="navbar">
+      <img src={assets.logo} alt="" className="logo" />
+
+      <ul className="navbar_menu">
+        <li>menu</li>
+        <li>home</li>
+        <li>mobile_app</li>
+        <li>contact us</li>
+      </ul>
+
+      <div className="navbar_right">
+        <img src={assets.search_icon} alt="" />
+
+        <div className="navbar_search_icon">
+          <img src={assets.basket_icon} alt="" />
+          <div className="dot"></div>
+        </div>
+
+        <button>sign in</button>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
