fe3c9a0fb9e459d3e002d4530c4db7588c2c78ac
Lê Võ Nhật Linh <levonhatlinh.work@gmail.com>
2025-11-12 11:19:33 +0700

    set up router

--- front-end/src/pages/Placeorder/PlaceOrder.jsx
+++ front-end/src/pages/Placeorder/PlaceOrder.jsx
@@ -1,7 +1,7 @@
 import React from "react";
-import "./PlacOrder.css";
-const Placeorder = () => {
+import "./PlaceOrder.css";
+const PlaceOrder = () => {
   return <div></div>;
 };
 
-export default Placeorder;
+export default PlaceOrder;
