8afcfc3dd2314f166ba6e61cc4cd9218e73c5883
Lê Võ Nhật Linh <levonhatlinh.work@gmail.com>
2025-11-17 12:14:26 +0700

    done foodlist

--- front-end/src/components/FoodDisplay/FoodDisplay.jsx
+++ front-end/src/components/FoodDisplay/FoodDisplay.jsx
@@ -3,7 +3,10 @@ import "./FoodDisplay.css";
 import { StoreContext } from "../../context/StoreContext";
 import FoodItem from "../FoodItem/FoodItem";
 
-const FoodDisplay = (category) => {
+// category là đại diện cho nhãn
+// category = "all" là nói về tất cả
+// category = "salad" là chỉ nói về salad
+const FoodDisplay = ({ category }) => {
   const { food_list } = useContext(StoreContext);
 
   return (
@@ -11,16 +14,21 @@ const FoodDisplay = (category) => {
       <h2>Top dishes near you</h2>
       <div className="food_display_list">
         {food_list.map((item, index) => {
-          return (
-            <FoodItem
-              key={index}
-              id={item._id}
-              name={item.name}
-              description={item.description}
-              price={item.price}
-              image={item.image}
-            />
-          );
+          // || là hoặc
+          // tức là category hiện tất cả hoặc category = item.category
+          // (category đã đươc phân loại sẵn ở trong asset)
+          if (category === "All" || category === item.category) {
+            return (
+              <FoodItem
+                key={index}
+                id={item._id}
+                name={item.name}
+                description={item.description}
+                price={item.price}
+                image={item.image}
+              />
+            );
+          }
         })}
       </div>
     </div>
--- front-end/src/context/StoreContext.jsx
+++ front-end/src/context/StoreContext.jsx
@@ -1,10 +1,38 @@
-import { createContext } from "react";
+import { createContext, useEffect, useState } from "react";
 import { food_list } from "../assets/assets";
 
 export const StoreContext = createContext(null);
+
 const StoreContextProvider = (props) => {
+  const [cartItems, setCartItems] = useState({});
+
+  // thêm vào
+  const addToCart = (itemId) => {
+    if (!cartItems[itemId]) {
+      
+      // prev[itemId] là giá chị cũ mới nhất của cartItems có id ..
+      //...prev: là copy tất cả cả gía trị cũ của cart items
+      setCartItems((prev) => ({ ...prev, [itemId]: 1 }));
+    } else {
+      setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }));
+    }
+  };
+
+  // bỏ bớt đi
+  const removeFromCart = (itemId) => {
+    setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }));
+  };
+
+  useEffect(() => {
+    console.log(cartItems);
+  }, [cartItems]);
+
   const contextValue = {
     food_list,
+    cartItems,
+    setCartItems,
+    addToCart,
+    removeFromCart,
   };
 
   return (
