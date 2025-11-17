bcf970ea5ed3629ec302ca7fcc71ce1565b2b3cb
Lê Võ Nhật Linh <levonhatlinh.work@gmail.com>
2025-11-17 10:56:54 +0700

    layout for fooditems

--- front-end/src/components/FoodItem/FoodItem.jsx
+++ front-end/src/components/FoodItem/FoodItem.jsx
@@ -7,7 +7,31 @@ const FoodItem = ({ id, name, price, description, image }) => {
     <div className="food_item">
       <div className="food_item_img_container">
         <img className="food_item_image" src={image} alt="" />
+
+        {!itemCount ? (
+          <img
+            className="add"
+            onClick={() => setItemCount((prev) => prev + 1)}
+            src={assets.add_icon_white}
+            alt=""
+          />
+        ) : (
+          <div className="food_item_counter">
+            <img
+              onClick={() => setItemCount((prev) => prev - 1)}
+              src={assets.remove_icon_red}
+              alt=""
+            />
+            <p>{itemCount}</p>
+            <img
+              onClick={() => setItemCount((prev) => prev + 1)}
+              src={assets.add_icon_green}
+              alt=""
+            />
+          </div>
+        )}
       </div>
+
       <div className="food_item_info">
         <div className="food_item_name_rating">
           <p>{name}</p>
