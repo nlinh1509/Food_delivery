f7ff70955e46569b2eca886dc668b44a04d77920
Lê Võ Nhật Linh <levonhatlinh.work@gmail.com>
2025-11-19 15:52:13 +0700

    totalcart

--- front-end/src/components/Navbar/Navbar.jsx
+++ front-end/src/components/Navbar/Navbar.jsx
@@ -1,39 +1,49 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import "./Navbar.css";
 import { assets } from "../../assets/assets";
 import { Link } from "react-router-dom";
+import { StoreContext } from "../../context/StoreContext";
 
-const Navbar = () => {
+const Navbar = ({ setShowLogin }) => {
   // useState là khởi tạo giá trị ban đầu khi mở web
   const [current_menu, update_menu] = useState("home");
+  
+  const{getTotalCartAmount} = useContext(StoreContext)
+  
   return (
     <div className="navbar">
-      <img src={assets.logo} alt="" className="logo" />
+      <Link to = '/'>
+        <img src={assets.logo} alt="" className="logo" />
+      </Link>
 
       <ul className="navbar_menu">
         {/* khi click vào home, update_menu là home */}
         {/* lúc này current_menu là home sẽ được active, 
         nếu k thì là rỗng (không có gì xảy ra)  */}
-        <Link to = '/'
+        <Link
+          to="/"
           onClick={() => update_menu("home")}
           className={current_menu === "home" ? "active" : ""}
         >
           home
         </Link>
-        <a href="#explore_menu"
+        <a
+          href="#explore_menu"
           onClick={() => update_menu("menu")}
           className={current_menu === "menu" ? "active" : ""}
         >
           menu
         </a>
-        
-        <a href="#app_download"
+
+        <a
+          href="#app_download"
           onClick={() => update_menu("mobile_app")}
           className={current_menu === "mobile_app" ? "active" : ""}
         >
           mobile-app
         </a>
-        <a href="#footer"
+        <a
+          href="#footer"
           onClick={() => update_menu("contact_us")}
           className={current_menu === "contact_us" ? "active" : ""}
         >
@@ -45,11 +55,14 @@ const Navbar = () => {
         <img src={assets.search_icon} alt="" />
 
         <div className="navbar_search_icon">
-          <img src={assets.basket_icon} alt="" />
-          <div className="dot"></div>
+          <Link to="/cart">
+            <img src={assets.basket_icon} alt="" />
+          </Link>
+
+          <div className={getTotalCartAmount()=== 0 ? "" :"dot"}></div>
         </div>
 
-        <button>sign in</button>
+        <button onClick={() => setShowLogin(true)}>sign in</button>
       </div>
     </div>
   );
--- front-end/src/pages/Cart/Cart.jsx
+++ front-end/src/pages/Cart/Cart.jsx
@@ -1,8 +1,76 @@
-import React from "react";
+import React, { useContext } from "react";
 import "./Cart.css";
+import { StoreContext } from "../../context/StoreContext";
 
 const Cart = () => {
-  return <div></div>;
+  const { cartItems, food_list, removeFromCart, getTotalCartAmount } = useContext(StoreContext);
+  return (
+    <div className="cart">
+      <div className="cart_items">
+        <div className="cart_items_title">
+          <p>Items</p>
+          <p>Title</p>
+          <p>Price</p>
+          <p>Quantity</p>
+          <p>Total</p>
+          <p>Remove</p>
+        </div>
+
+        <br />
+        <hr />
+        {food_list.map((item, index) => {
+          if (cartItems[item._id] > 0) {
+            return (
+              <div>
+                <div className="cart_items_title cart_items_item">
+                  <img src={item.image} alt="" />
+                  <p>{item.name}</p>
+                  <p>${item.price}</p>
+                  <p>{cartItems[item._id]}</p>
+                  <p>${item.price * cartItems[item._id]}</p>
+                  <p onClick={() => removeFromCart(item._id)} className="cross">
+                    x
+                  </p>
+                </div>
+                <hr />
+              </div>
+            );
+          }
+        })}
+      </div>
+      <div className="cart_bottom">
+        <div className="cart_total">
+          <h2>Cart Totals</h2>
+          <div>
+            <div className="cart_total_details">
+              <p>Subtotal</p>
+              <p>${getTotalCartAmount()}</p>
+            </div>
+            <hr />
+            <div className="cart_total_details">
+              <p>Delivery Fee</p>
+              <p>${2}</p>
+            </div>
+            <hr />
+            <div className="cart_total_details">
+              <b>Total</b>
+              <b>${getTotalCartAmount() +2}</b>
+            </div>
+          </div>
+          <button>PROCEED TO CHECKOUT</button>
+        </div>
+        <div className="cart_promocode">
+          <div>
+            <p>If you have a promo code. Enter it here</p>
+            <div className="cart_promocode_inputs">
+              <input type="text" placeholder="promo code" />
+              <button>Submit</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Cart;
