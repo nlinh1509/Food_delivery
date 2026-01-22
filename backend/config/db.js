d623cb01792613187dc6d70cc3ce4b1c8ebcc669
Lê Võ Nhật Linh <levonhatlinh.work@gmail.com>
2026-01-22 10:22:09 +0700

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 export const connectDB = async () => {
   await mongoose
     .connect(
-      "mongodb+srv://nlinh:nlinh1509@cluster0.c56krn3.mongodb.net/food-del"
+      ""
     )
     .then(() => console.log("DB Connected"));
 };
